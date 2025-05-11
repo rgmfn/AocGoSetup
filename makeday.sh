eb5f3174087e8c7211b43dce7499a57ed4a0ce52
Ryan Gragg <rygragg@gmail.com>
2025-05-11 11:08:58 -0700

    dont make my username default in module name

--- makeday.sh
+++ makeday.sh
@@ -1,14 +1,18 @@
-if [ -z "$1" ]; then
+YEAR="2023"
+USERNAME="your user here"
+
+if [ "$USERNAME" = "your user here" ]; then
+    echo "Please change the value of the USERNAME variable."
+    exit 1
+elif [ -z "$1" ]; then
     echo "ERROR: No day given"
     exit 1
 fi
 
-YEAR="2023"
-
 mkdir "$1"
 cp ./.blank/** "./$1"
 
-echo "module github.com/rgmfn/aoc$YEAR/$1
+echo "module github.com/$USERNAME/aoc$YEAR/$1
 
 go 1.20" >> "./$1/go.mod"
 touch "./$1/test.txt"
